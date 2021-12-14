e057fe4197274afc98c327dcad3ba9ccfc92366e
Eric Buss <root@ejrbuss.net>
2021-12-14 15:22:51 -0800

    day 14

--- src/day14.ts
+++ src/day14.ts
@@ -1,165 +1,70 @@
 import { challenge, max, min, parseLines } from "./common.js";
 
-type Encoding = string[];
+type Rules = Record<string, string>;
+type CharCounts = Record<string, number>;
 
-function createEncoding(charSet: Set<string>): Encoding {
-	return Array.from(charSet);
+function parseInput(input: string): {
+	template: string;
+	rules: Rules;
+} {
+	const lines = parseLines(input);
+	const template = lines.shift() as string;
+	const rules: Rules = {};
+	for (const line of lines) {
+		const [left, right] = line.split("->").map((part) => part.trim());
+		rules[left] = right;
+	}
+	return { template, rules };
 }
 
-function encode(encoding: Encoding, char: string): number {
-	return encoding.indexOf(char) + 1;
-}
+function checksum(template: string, rules: Rules, generations: number) {
+	const pairCharCounts: Record<string, CharCounts> = {};
 
-const ChunkSize = 1 << 20;
-
-type CodeString = {
-	currentChunk: number;
-	currentOffset: number;
-	chunks: Uint8Array[];
-};
-
-function createCodeString(): CodeString {
-	return {
-		currentChunk: 0,
-		currentOffset: 0,
-		chunks: [new Uint8Array(ChunkSize)],
-	};
-}
-
-function reset(codeString: CodeString) {
-	codeString.currentChunk = 0;
-	codeString.currentOffset = 0;
-}
-
-function appendCode(codeString: CodeString, code: number) {
-	codeString.chunks[codeString.currentChunk][codeString.currentOffset++] = code;
-	if (codeString.currentOffset === ChunkSize) {
-		codeString.currentChunk += 1;
-		codeString.currentOffset = 0;
-		if (codeString.currentChunk >= codeString.chunks.length) {
-			codeString.chunks.push(new Uint8Array(ChunkSize));
+	function recurse(pair: string, generation: number): CharCounts {
+		const key = pair + "_" + generation;
+		if (key in pairCharCounts) {
+			return pairCharCounts[key];
 		}
+		if (generation === generations || !(pair in rules)) {
+			return (pairCharCounts[key] = { [pair[0]]: 1 });
+		}
+		const insertion = rules[pair];
+		const pair1 = pair[0] + insertion;
+		const pair2 = insertion + pair[1];
+		const charCounts = addCounts(
+			recurse(pair1, generation + 1),
+			recurse(pair2, generation + 1)
+		);
+		return (pairCharCounts[key] = charCounts);
 	}
-}
 
-function getCode(codeString: CodeString, position: number): number {
-	const chunk = Math.floor(position / ChunkSize);
-	const offset = position % ChunkSize;
-	return codeString.chunks[chunk][offset];
-}
-
-function lengthOfCodeString(codeString: CodeString): number {
-	return ChunkSize * codeString.currentChunk + codeString.currentOffset;
-}
-
-function checksum(codeString: CodeString) {
-	const length = lengthOfCodeString(codeString);
-	const codeCounts: Record<number, number> = {};
-	for (let i = 0; i < length; i += 1) {
-		const char = getCode(codeString, i);
-		codeCounts[char] = (codeCounts[char] ?? 0) + 1;
+	let charCounts: CharCounts = {};
+	for (let i = 0; i + 1 < template.length; i += 1) {
+		const pair = template.substring(i, i + 2);
+		charCounts = addCounts(charCounts, recurse(pair, 0));
 	}
-	const counts = Object.values(codeCounts);
+	charCounts = addCounts(charCounts, { [template[template.length - 1]]: 1 });
+	const counts = Object.values(charCounts);
 	return max(counts) - min(counts);
 }
 
-type PairRules = Record<number, number>;
-
-function createPairRules(): PairRules {
-	return {};
-}
-
-function addRule(
-	pairRules: PairRules,
-	left: number,
-	right: number,
-	insertion: number
-) {
-	pairRules[left | (right << 4)] = insertion;
-}
-
-function getInsertion(
-	pairRules: PairRules,
-	left: number,
-	right: number
-): number | undefined {
-	return pairRules[left | (right << 4)];
-}
-
-function parseInput(input: string): {
-	template: CodeString;
-	pairRules: PairRules;
-} {
-	const lines = parseLines(input);
-	const stringTemplate = lines.shift() as string;
-	const charSet: Set<string> = new Set();
-	for (const line of lines) {
-		const [left, right, insertion] = line.replace(/\s|->/g, "").split("");
-		charSet.add(left);
-		charSet.add(right);
-		charSet.add(insertion);
+function addCounts(charCounts1: CharCounts, charCounts2: CharCounts) {
+	const sumCharCounts: CharCounts = {};
+	for (const char in charCounts1) {
+		sumCharCounts[char] = (sumCharCounts[char] ?? 0) + charCounts1[char];
 	}
-	const encoding = createEncoding(charSet);
-	const template = createCodeString();
-	const pairRules = createPairRules();
-	for (const char of Array.from(stringTemplate)) {
-		appendCode(template, encode(encoding, char));
+	for (const char in charCounts2) {
+		sumCharCounts[char] = (sumCharCounts[char] ?? 0) + charCounts2[char];
 	}
-	for (const line of lines) {
-		const [left, right, insertion] = line.replace(/\s|->/g, "").split("");
-		addRule(
-			pairRules,
-			encode(encoding, left),
-			encode(encoding, right),
-			encode(encoding, insertion)
-		);
-	}
-	return { template, pairRules };
-}
-
-function step(
-	inString: CodeString,
-	outString: CodeString,
-	pairRules: PairRules
-) {
-	const length = lengthOfCodeString(inString);
-	let previousCode = getCode(inString, 0);
-	appendCode(outString, previousCode);
-	for (let i = 1; i < length; i += 1) {
-		const code = getCode(inString, i);
-		const insertion = getInsertion(pairRules, previousCode, code);
-		if (insertion !== undefined) {
-			appendCode(outString, insertion);
-		}
-		appendCode(outString, code);
-		previousCode = code;
-	}
+	return sumCharCounts;
 }
 
 challenge({ day: 14, part: 1 }, (input) => {
-	let { template, pairRules } = parseInput(input);
-	let inString = template;
-	let outString = createCodeString();
-	for (let i = 0; i < 10; i += 1) {
-		step(inString, outString, pairRules);
-		const tmp = inString;
-		inString = outString;
-		outString = tmp;
-		reset(outString);
-	}
-	return checksum(inString);
+	let { template, rules } = parseInput(input);
+	return checksum(template, rules, 10);
 });
 
 challenge({ day: 14, part: 2 }, (input) => {
-	let { template, pairRules } = parseInput(input);
-	let inString = template;
-	let outString = createCodeString();
-	for (let i = 0; i < 40; i += 1) {
-		console.log(`Step: ${i} Length: ${lengthOfCodeString(inString)}`);
-		step(inString, outString, pairRules);
-		const tmp = inString;
-		inString = outString;
-		outString = tmp;
-	}
-	return checksum(inString);
+	let { template, rules } = parseInput(input);
+	return checksum(template, rules, 40);
 });
